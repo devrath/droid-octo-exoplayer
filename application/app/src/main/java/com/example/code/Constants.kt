8bcaf08a925a9e381e0f68b5921e1b0f888535bb
Devrath <devrath.dev595@gmail.com>
2021-10-12 18:04:24 +0530

    v

--- application/app/src/main/java/com/example/code/Constants.kt
+++ application/app/src/main/java/com/example/code/Constants.kt
@@ -3,4 +3,7 @@ package com.example.code
 object Constants {
     const val mp4Url = "https://i.imgur.com/7bMqysJ.mp4"
     const val dashUrl = "https://storage.googleapis.com/wvmedia/clear/vp9/tears/tears_uhd.mpd"
+
+    const val MEDIA_SOURCE_DASH = "dash"
+    const val MEDIA_SOURCE_MP4 = "mp4"
 }
--- application/app/src/main/java/com/example/code/MainActivity.kt
+++ application/app/src/main/java/com/example/code/MainActivity.kt
@@ -7,10 +7,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.example.code.Constants.MEDIA_SOURCE_DASH
+import com.example.code.Constants.MEDIA_SOURCE_MP4
 import com.example.code.Constants.dashUrl
 import com.example.code.Constants.mp4Url
 import com.example.code.databinding.ActivityMainBinding
 import com.google.android.exoplayer2.ExoPlaybackException
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
@@ -19,8 +22,9 @@ import com.google.android.exoplayer2.source.dash.DashMediaSource
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity(), Player.EventListener {
+class MainActivity : AppCompatActivity(), Player.Listener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -57,16 +61,6 @@ class MainActivity : AppCompatActivity(), Player.EventListener {
         releasePlayer()
     }
 
-    private fun initializePlayer() {
-        simpleExoplayer = SimpleExoPlayer.Builder(this).build()
-        val randomUrl = urlList.random()
-        preparePlayer(randomUrl.first, randomUrl.second)
-        binding.exoplayerView.player = simpleExoplayer
-        simpleExoplayer.seekTo(playbackPosition)
-        simpleExoplayer.playWhenReady = true
-        simpleExoplayer.addListener(this)
-    }
-
     private fun buildMediaSource(uri: Uri, type: String): MediaSource {
         return if (type == "dash") {
             DashMediaSource.Factory(dataSourceFactory)
@@ -78,9 +72,15 @@ class MainActivity : AppCompatActivity(), Player.EventListener {
     }
 
     private fun preparePlayer(videoUrl: String, type: String) {
+        // Url from which video is playing
         val uri = Uri.parse(videoUrl)
+        // Determining the type of media source
         val mediaSource = buildMediaSource(uri, type)
-        simpleExoplayer.prepare(mediaSource)
+        // Apply the params to the exo player
+        simpleExoplayer.apply {
+            setMediaSource(mediaSource)
+            prepare()
+        }
     }
 
     private fun releasePlayer() {
@@ -96,27 +96,21 @@ class MainActivity : AppCompatActivity(), Player.EventListener {
     }
 
 
-    fun selectUrl(context: Context){
+    private fun selectUrl(context: Context){
         // setup alert builder
         val builder = MaterialAlertDialogBuilder(context)
-        builder.setTitle("Choose an Item")
+        builder.setTitle(getString(R.string.choose_video_format_to_play))
 
         // add list items
-        val listItems = arrayOf("Mp4-URL","Dash-URL","Item 2")
+        val listItems = resources.getStringArray(R.array.videoFormatsSelectionList)
+
         builder.setItems(listItems) { dialog, which ->
             when (which) {
-                0 ->{
-                    initPlayer(mp4Url)
-                    Toast.makeText(context,"You Clicked Item 0",Toast.LENGTH_LONG).show()
-                    dialog.dismiss()
-                }
-                1->{
-                    initPlayer(dashUrl)
-                    Toast.makeText(context,"You Clicked Item 1",Toast.LENGTH_LONG).show()
-                    dialog.dismiss()
-                }
+                0 -> initPlayer(mp4Url,MEDIA_SOURCE_MP4)
+                1 -> initPlayer(dashUrl, MEDIA_SOURCE_DASH)
                 else -> Toast.makeText(context,"Invalid",Toast.LENGTH_LONG).show()
             }
+            dialog.dismiss()
         }
 
         // create & show alert dialog
@@ -124,16 +118,22 @@ class MainActivity : AppCompatActivity(), Player.EventListener {
         dialog.show()
     }
 
-    private fun initPlayer(url: String) {
+    private fun initPlayer(url: String,type: String) {
         releasePlayer()
-        initializePlayer(url)
+        initializePlayer(url,type)
     }
 
-    private fun initializePlayer(url: String) {
+    /**
+     *
+     */
+    private fun initializePlayer(
+        url: String=mp4Url,
+        type: String=MEDIA_SOURCE_MP4
+    ) {
         simpleExoplayer = SimpleExoPlayer.Builder(this).build()
-        val randomUrl = urlList.random()
-        preparePlayer(randomUrl.first, randomUrl.second)
+        preparePlayer(url, type)
         binding.exoplayerView.player = simpleExoplayer
+        // Set to initial position
         simpleExoplayer.seekTo(playbackPosition)
         simpleExoplayer.playWhenReady = true
         simpleExoplayer.addListener(this)
