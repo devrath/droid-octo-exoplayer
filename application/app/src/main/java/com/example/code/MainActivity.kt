e4f93d24b00de55c3894d140c3e4be625cf021f4
Devrath <devrath.dev595@gmail.com>
2021-10-13 18:32:54 +0530

    m

--- application/app/src/main/java/com/example/code/MainActivity.kt
+++ application/app/src/main/java/com/example/code/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.code
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.code.databinding.ActivityMainBinding
-import com.example.code.exoplayer.ExoPlayerActivity
-import com.google.android.exoplayer2.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.launchPlayerId.setOnClickListener {
-            startActivity(Intent(this@MainActivity, ExoPlayerActivity::class.java))
-        }
     }
 
 }
--- application/exoplayer/src/main/java/com/example/code/exoplayer/ExoPlayerFragment.kt
+++ application/exoplayer/src/main/java/com/example/code/exoplayer/ExoPlayerFragment.kt
@@ -1,28 +1,18 @@
 package com.example.code.exoplayer
 
-import android.content.ContentValues
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.example.code.exoplayer.Constants.dashUrl
-import com.example.code.exoplayer.Constants.mp3Url
-import com.example.code.exoplayer.Constants.mp4Url
+import androidx.fragment.app.Fragment
 import com.example.code.exoplayer.databinding.ActivityExoplayerBinding
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.upstream.DataSource
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.MimeTypes
-import com.google.android.exoplayer2.util.Util
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-class ExoPlayerActivity : AppCompatActivity(), Player.Listener {
+class ExoPlayerFragment : Fragment(), Player.Listener {
 
     private val binding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityExoplayerBinding.inflate(layoutInflater)
@@ -30,16 +20,23 @@ class ExoPlayerActivity : AppCompatActivity(), Player.Listener {
 
     private lateinit var locationListener: ExoplayerLifecycleObserver
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setOnClickListener()
         initExoplayerListener()
     }
 
     private fun setOnClickListener() {
         binding.selectUrlId.setOnClickListener {
-            selectUrl(this@ExoPlayerActivity)
+            activity?.let{ selectUrl(it) }
         }
     }
 
@@ -53,9 +50,9 @@ class ExoPlayerActivity : AppCompatActivity(), Player.Listener {
 
         builder.setItems(listItems) { dialog, which ->
             when (which) {
-                0 -> locationListener.changeTrack(mp4Url, MimeTypes.APPLICATION_MP4)
-                1 -> locationListener.changeTrack(dashUrl, MimeTypes.APPLICATION_MPD)
-                2 -> locationListener.changeTrack(mp3Url, MimeTypes.APPLICATION_MP4)
+                0 -> locationListener.changeTrack(Constants.mp4Url, MimeTypes.APPLICATION_MP4)
+                1 -> locationListener.changeTrack(Constants.dashUrl, MimeTypes.APPLICATION_MPD)
+                2 -> locationListener.changeTrack(Constants.mp3Url, MimeTypes.APPLICATION_MP4)
                 else -> Toast.makeText(context,"Invalid", Toast.LENGTH_LONG).show()
             }
             dialog.dismiss()
@@ -67,10 +64,12 @@ class ExoPlayerActivity : AppCompatActivity(), Player.Listener {
     }
 
     private fun initExoplayerListener() {
-        locationListener = ExoplayerLifecycleObserver(lifecycle,this) {
-            when(it) {
-                is ExoplayerAction.BindExoplayer -> binding.exoplayerView.player = it.simpleExoplayer
-                is ExoplayerAction.ProgressBarVisibility -> handleProgressVisibilityOfPlayer(it.isVisible)
+        activity?.let{
+            locationListener = ExoplayerLifecycleObserver(lifecycle,it) {
+                when(it) {
+                    is ExoplayerAction.BindExoplayer -> binding.exoplayerView.player = it.simpleExoplayer
+                    is ExoplayerAction.ProgressBarVisibility -> handleProgressVisibilityOfPlayer(it.isVisible)
+                }
             }
         }
     }
@@ -82,5 +81,6 @@ class ExoPlayerActivity : AppCompatActivity(), Player.Listener {
         else
             binding.progressBar.visibility = View.INVISIBLE
     }
+
 
 }
