ba4b42c41e64c103e5b47f50962093a5e6431a19
Devrath <devrath.dev595@gmail.com>
2021-10-13 18:20:21 +0530

    v

---
+++ application/extensions/src/main/java/com/example/code/extensions/FragmentExt.kt
@@ -0,0 +1,73 @@
+package com.example.code.extensions
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+fun AppCompatActivity.popBackStack() {
+    this.hideSoftKeyboard()
+    supportFragmentManager.popBackStack()
+}
+
+fun AppCompatActivity.popBackStackInclusive() {
+    this.hideSoftKeyboard()
+    if (supportFragmentManager.backStackEntryCount > 0)
+        supportFragmentManager.popBackStack(supportFragmentManager.getBackStackEntryAt(0).id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+}
+
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().func().commit()
+}
+
+
+fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int) {
+    supportFragmentManager.inTransaction { add(frameId, fragment, fragment.javaClass.simpleName) }
+}
+
+fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int) {
+    supportFragmentManager.inTransaction { replace(frameId, fragment, fragment.javaClass.simpleName) }
+}
+
+fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int, addToStack: Boolean) {
+    supportFragmentManager.inTransaction {
+        if (addToStack) replace(frameId, fragment, fragment.javaClass.simpleName)
+            .addToBackStack(fragment.javaClass.simpleName)
+        else
+            replace(frameId, fragment, fragment.javaClass.simpleName)
+    }
+}
+
+fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int, addToStack: Boolean, clearBackStack: Boolean) {
+    supportFragmentManager.inTransaction {
+
+        if (clearBackStack && supportFragmentManager.backStackEntryCount > 0) {
+            val first = supportFragmentManager.getBackStackEntryAt(0)
+            supportFragmentManager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
+
+        if (addToStack) replace(frameId, fragment, fragment.javaClass.simpleName)
+            .addToBackStack(fragment.javaClass.simpleName)
+        else
+            replace(frameId, fragment, fragment.javaClass.simpleName)
+    }
+}
+
+fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int, addToStack: Boolean) {
+    supportFragmentManager.inTransaction {
+        if (addToStack) add(frameId, fragment, fragment.javaClass.simpleName)
+            .addToBackStack(fragment.javaClass.simpleName)
+        else add(frameId, fragment)
+    }
+}
+
+
+fun AppCompatActivity.getCurrentFragment(): Fragment? {
+    val fragmentManager = supportFragmentManager
+    var fragmentTag: String? = ""
+
+    if (fragmentManager.backStackEntryCount > 0)
+        fragmentTag = fragmentManager.getBackStackEntryAt(fragmentManager.backStackEntryCount - 1).name
+
+    return fragmentManager.findFragmentByTag(fragmentTag)
+}
---
+++ application/extensions/src/main/java/com/example/code/extensions/KeyboardExt.kt
@@ -0,0 +1,41 @@
+package com.example.code.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
+
+/**
+ * Extension method to provide show keyboard for View.
+ */
+fun View.showKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    this.requestFocus()
+    imm.showSoftInput(this, 0)
+}
+
+/**
+ * Extension method to provide hide keyboard for [Activity].
+ */
+fun Activity.hideSoftKeyboard() {
+    if (currentFocus != null) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+    }
+}
+
+/**
+ * Extension method to provide hide keyboard for [Fragment].
+ */
+fun Fragment.hideSoftKeyboard() {
+    activity?.hideSoftKeyboard()
+}
+
+/**
+ * Extension method to provide hide keyboard for [View].
+ */
+fun View.hideKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
+}
