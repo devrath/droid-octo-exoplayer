7fe35b75527948b92edcd71fd8dd1516570f0c64
Devrath <devrath.dev595@gmail.com>
2021-10-16 21:34:19 +0530

    n

--- application/app/src/main/java/com/example/code/exoplayers/StyledExoPlayerActivity.kt
+++ application/app/src/main/java/com/example/code/exoplayers/StyledExoPlayerActivity.kt
@@ -3,13 +3,14 @@ package com.example.code.exoplayers
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.code.databinding.ActivitySimpleExoPlayerBinding
+import com.example.code.databinding.ActivityStyledExoPlayerBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class StyledExoPlayerActivity : AppCompatActivity() {
 
     private val binding by lazy(LazyThreadSafetyMode.NONE) {
-        ActivitySimpleExoPlayerBinding.inflate(layoutInflater)
+        ActivityStyledExoPlayerBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
