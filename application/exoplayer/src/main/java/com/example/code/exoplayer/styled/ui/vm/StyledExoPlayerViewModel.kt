45deb4cecc57134116446bad7dbdede3651d4d4e
Devrath <devrath.dev595@gmail.com>
2021-10-17 22:12:16 +0530

    m

--- application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/vm/StyledExoPlayerViewModel.kt
+++ application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/vm/StyledExoPlayerViewModel.kt
@@ -2,13 +2,15 @@ package com.example.code.exoplayer.styled.ui.vm
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.example.code.exoplayer.styled.core.StyledExoPlayer
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 @HiltViewModel
 class StyledExoPlayerViewModel @Inject constructor(
-    @ApplicationContext context: Context
+    @ApplicationContext context: Context,
+    private val player : StyledExoPlayer
 ) : ViewModel() {
 
     fun test(): String {
