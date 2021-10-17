c377798f7f9871a0f4e96ccf928d83f9018675c0
Devrath <devrath.dev595@gmail.com>
2021-10-17 18:15:15 +0530

    v

--- application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/view/StyledExoPlayerFragment.kt
+++ application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/view/StyledExoPlayerFragment.kt
@@ -2,19 +2,36 @@ package com.example.code.exoplayer.styled.ui.view
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.example.code.exoplayer.databinding.FragmentAddsExoPlayerBinding
 import com.example.code.exoplayer.databinding.FragmentStyledExoPlayerBinding
+import com.example.code.exoplayer.styled.ui.vm.StyledExoPlayerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class StyledExoPlayerFragment : BaseFragment<FragmentStyledExoPlayerBinding>(FragmentStyledExoPlayerBinding::inflate) {
+class StyledExoPlayerFragment : Fragment() {
 
     private lateinit var mContext : Context
 
+    private val viewModel : StyledExoPlayerViewModel by viewModels()
+
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        FragmentStyledExoPlayerBinding.inflate(layoutInflater)
+    }
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        mContext = context
+        mContext=context
     }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View { return binding.root }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/vm/StyledExoPlayerViewModel.kt
+++ application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/vm/StyledExoPlayerViewModel.kt
@@ -1,8 +1,19 @@
 package com.example.code.exoplayer.styled.ui.vm
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class StyledExoPlayerViewModel  : ViewModel() {
+@HiltViewModel
+class StyledExoPlayerViewModel @Inject constructor(
+    @ApplicationContext context: Context
+) : ViewModel() {
+
+    fun test(): String {
+        return "Hello"
+    }
 
 
 }
