a972b5d43dee1a9083374e42184faf844c76de33
Devrath <devrath.dev595@gmail.com>
2021-10-19 12:46:27 +0530

    v

--- application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/viewAction/ExoPlayerAction.kt
+++ application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/viewAction/ExoPlayerAction.kt
@@ -2,6 +2,7 @@ package com.example.code.exoplayer.styled.ui.viewAction
 
 import com.example.code.exoplayer.styled.util.LiveStatus
 import com.example.code.exoplayer.styled.util.MplTrack
+import com.example.code.exoplayer.styled.util.MplTrackList
 
 sealed class ExoPlayerAction {
 
@@ -10,15 +11,19 @@ sealed class ExoPlayerAction {
     class ShowReplay(val show: Boolean) : ExoPlayerAction()
     class ShowPlayPause(val show: Boolean) : ExoPlayerAction()
     class SetForwardIconVisibility(val visible : Boolean) : ExoPlayerAction()
-   /* class ShowSnackBar(val text : String) : ExoPlayerAction()
-    object DismissVideoQualityBottomSheet : ExoPlayerAction()
     object FirstFrameRendered : ExoPlayerAction()
-    class ReminderError(val message: String?, val flag: Boolean) : ExoPlayerAction()
-    class ReminderSuccess(val flag: Boolean) : ExoPlayerAction()
-    class ShowError(val error : String, val currentPosition: Long?) : ExoPlayerAction()
+    class EnableTracks(val enable: Boolean) : ExoPlayerAction()
+    class TracksChange(val tracks: MplTrackList?) : ExoPlayerAction()
     class LiveView(val liveStatus: LiveStatus) : ExoPlayerAction()
+    object DismissVideoQualityBottomSheet : ExoPlayerAction()
+    class ShowSnackBar(val text : String) : ExoPlayerAction()
+    class ShowError(val error : String, val currentPosition: Long?) : ExoPlayerAction()
     class SelectedTrack(val track: MplTrack?) : ExoPlayerAction()
-    class EnableTracks(val enable: Boolean) : ExoPlayerAction()*/
+
+    /*
+     class ReminderError(val message: String?, val flag: Boolean) : ExoPlayerAction()
+     class ReminderSuccess(val flag: Boolean) : ExoPlayerAction()
+     */
 
 }
 
--- application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/vm/StyledExoPlayerViewModel.kt
+++ application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/vm/StyledExoPlayerViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.code.exoplayer.styled.ui.vm
 
 import android.content.Context
+import android.os.Looper
 import android.webkit.URLUtil.isHttpUrl
 import android.webkit.URLUtil.isHttpsUrl
 import androidx.lifecycle.MutableLiveData
@@ -9,9 +10,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.code.exoplayer.Constants.dashUrl
 import com.example.code.exoplayer.styled.core.StyledExoPlayer
 import com.example.code.exoplayer.styled.ui.viewAction.ExoPlayerAction
-import com.example.code.exoplayer.styled.util.MplTrack
-import com.example.code.exoplayer.styled.util.MplTrackList
-import com.example.code.exoplayer.styled.util.MplVideo
+import com.example.code.exoplayer.styled.util.*
 import com.google.android.exoplayer2.Player
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -28,6 +27,9 @@ class StyledExoPlayerViewModel @Inject constructor(
     private val tag = this.javaClass.simpleName
     private lateinit var mplVideo: MplVideo
     val command: MutableLiveData<ExoPlayerAction> = MutableLiveData()
+    val exoPlayerLiveData: MutableLiveData<StyledExoPlayer> = MutableLiveData()
+    private var tracksList: MplTrackList? = null
+    var currentBroadcast: Broadcast? = null
 
     init { setMplVideo() }
 
@@ -72,8 +74,6 @@ class StyledExoPlayerViewModel @Inject constructor(
         return isValidUrl
     }
 
-
-
     private val eventListener: StyledExoPlayer.EventListener = object : StyledExoPlayer.EventListener {
 
         override fun onPlaybackStateChanged(state: Int) {
@@ -83,26 +83,37 @@ class StyledExoPlayerViewModel @Inject constructor(
 
         override fun onLiveStateChanged(isInSyncWithLive: Boolean, hasEndTag: Boolean) {
             Timber.tag(tag).d("onLiveStateChanged")
+            onLiveStateChangedListener(isInSyncWithLive, hasEndTag)
         }
 
         override fun changeForwardIconVisibility(visible: Boolean) {
             Timber.tag(tag).d("changeForwardIconVisibility")
+            command.value = ExoPlayerAction.SetForwardIconVisibility(visible)
         }
 
         override fun onTracksChanged(tracksList: MplTrackList) {
             Timber.tag(tag).d("onTracksChanged : $tracksList")
+            this@StyledExoPlayerViewModel.tracksList = tracksList
+            command.value = ExoPlayerAction.TracksChange(tracksList)
+            command.value = ExoPlayerAction.EnableTracks(tracksList.size > 0)
         }
 
         override fun onTracksSelected(success: Boolean, isSeamless: Boolean, startTime: Long, mplTrack: MplTrack) {
             Timber.tag(tag).d("onTracksSelected")
+            command.value = ExoPlayerAction.DismissVideoQualityBottomSheet
+            if (isSeamless) {
+                command.value = ExoPlayerAction.ShowSnackBar("The selected resolution will be applied in a few seconds")
+            }
         }
 
         override fun onInitDone() {
             Timber.tag(tag).d("onInitDone")
+            exoPlayerLiveData.value = player
         }
 
         override fun onError(type: Int, error : String?, currentPosition: Long?) {
             Timber.tag(tag).d("onError")
+            command.value = ExoPlayerAction.ShowError("[Type : $type, Error : ${error ?: "null"}", currentPosition)
         }
 
     }
@@ -119,6 +130,7 @@ class StyledExoPlayerViewModel @Inject constructor(
 
         override fun onFirsFrameRendered() {
             Timber.tag(tag).d("onFirsFrameRendered")
+            command.value = ExoPlayerAction.FirstFrameRendered
         }
 
         override fun sendBroadcastViewedEvent() {
@@ -156,6 +168,24 @@ class StyledExoPlayerViewModel @Inject constructor(
                     command.value = ExoPlayerAction.ShowReplay(show = false)
                     command.value = ExoPlayerAction.ShowPlayPause(show = true)
                 }
+            }
+        }
+    }
+
+    fun onLiveStateChangedListener(isInSyncWithLive: Boolean, hasEndTag: Boolean) {
+        if (hasEndTag || currentBroadcast?.isVOD() == true) {
+            command.value =
+                ExoPlayerAction.LiveView(LiveStatus(isVOD = true, isCurrentPositionLive = false))
+        } else {
+            // Live
+            if (isInSyncWithLive) {
+                command.value = ExoPlayerAction.LiveView(
+                    LiveStatus(isVOD = false, isCurrentPositionLive = true)
+                )
+            } else {
+                command.value = ExoPlayerAction.LiveView(
+                    LiveStatus(isVOD = false, isCurrentPositionLive = false)
+                )
             }
         }
     }
