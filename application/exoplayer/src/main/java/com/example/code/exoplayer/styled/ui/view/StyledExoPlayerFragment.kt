602c91a770244087aa33832a1713d87916e2ab87
Devrath <devrath.dev595@gmail.com>
2021-10-19 13:10:13 +0530

    v

--- application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/view/StyledExoPlayerFragment.kt
+++ application/exoplayer/src/main/java/com/example/code/exoplayer/styled/ui/view/StyledExoPlayerFragment.kt
@@ -2,13 +2,12 @@ package com.example.code.exoplayer.styled.ui.view
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.*
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import com.example.code.exoplayer.databinding.FragmentAddsExoPlayerBinding
+import androidx.lifecycle.Observer
 import com.example.code.exoplayer.databinding.FragmentStyledExoPlayerBinding
 import com.example.code.exoplayer.styled.ui.viewAction.ExoPlayerAction
 import com.example.code.exoplayer.styled.ui.vm.StyledExoPlayerViewModel
@@ -18,9 +17,9 @@ import timber.log.Timber
 @AndroidEntryPoint
 class StyledExoPlayerFragment : Fragment() {
 
-    private lateinit var mContext : Context
+    private lateinit var mContext: Context
     private val tAG = this.javaClass.simpleName
-    private val viewModel : StyledExoPlayerViewModel by viewModels()
+    private val viewModel: StyledExoPlayerViewModel by viewModels()
 
     private val binding by lazy(LazyThreadSafetyMode.NONE) {
         FragmentStyledExoPlayerBinding.inflate(layoutInflater)
@@ -28,12 +27,15 @@ class StyledExoPlayerFragment : Fragment() {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        mContext=context
+        mContext = context
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View { return binding.root }
+    ): View {
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -44,8 +46,32 @@ class StyledExoPlayerFragment : Fragment() {
     private fun registerObservers() {
         viewModel.command.observe(viewLifecycleOwner, {
             Timber.tag(tAG).d("OnChange $it")
-            when(it) {
+            when (it) {
                 is ExoPlayerAction.Validation -> validation(it)
+                is ExoPlayerAction.DismissVideoQualityBottomSheet ->
+                    Timber.tag(tAG).d("DismissVideoQualityBottomSheet")
+                is ExoPlayerAction.EnableTracks -> Timber.tag(tAG).d("EnableTracks")
+                is ExoPlayerAction.FirstFrameRendered ->
+                    Timber.tag(tAG).d("FirstFrameRendered")
+                is ExoPlayerAction.LiveView -> Timber.tag(tAG).d("LiveView")
+                is ExoPlayerAction.Progressbar -> Timber.tag(tAG).d("Progressbar")
+                is ExoPlayerAction.SelectedTrack -> Timber.tag(tAG).d("SelectedTrack")
+                is ExoPlayerAction.SetForwardIconVisibility ->
+                    Timber.tag(tAG).d("SetForwardIconVisibility")
+                is ExoPlayerAction.ShowError -> Timber.tag(tAG).d("ShowError")
+                is ExoPlayerAction.ShowPlayPause -> Timber.tag(tAG).d("ShowPlayPause")
+                is ExoPlayerAction.ShowReplay -> Timber.tag(tAG).d("ShowReplay")
+                is ExoPlayerAction.ShowSnackBar -> Timber.tag(tAG).d("ShowSnackBar")
+                is ExoPlayerAction.TracksChange -> Timber.tag(tAG).d("TracksChange")
+            }
+        })
+
+        viewModel.exoPlayerLiveData.observe(viewLifecycleOwner, Observer { it ->
+            Timber.tag(tAG).d("AddLifecycleToExoPlayer ${it.player}")
+            lifecycle.addObserver(it)
+            it.player?.let {
+                Timber.tag(tAG).d("Exoplayer setPlayer")
+                binding.exoplayerView.setPlayer(it)
             }
         })
     }
